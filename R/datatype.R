60943b4dea50c36ceb1c9a89d0dc4d8d135ced04
avahoffman <avamariehoffman@gmail.com>
2022-12-14 14:19:38 -0500

    Get rid of debugging comment

--- R/datatype.R
+++ R/datatype.R
@@ -1,21 +1,20 @@
 #' Title
 #'
+#' @param input
+#'
 #' @return
 #' @export
 #'
 #' @examples
 datatype_comment <- function(input) {
-  print(input$datatype_comment)
-  print(class(input$datatype_comment))
-
-  if(input$datatype_comment != "")
-  dt_comment <- paste0(
-    "<font color='OA799A'>",
-    input$datatype_comment,
-    "</font>"
-  ) else {
+  if (input$datatype_comment != "")
+    dt_comment <-
+      paste0("<font color='OA799A'>",
+             input$datatype_comment,
+             "</font>"
+      )
+  else {
     dt_comment <- ""
   }
-
   return(dt_comment)
 }
