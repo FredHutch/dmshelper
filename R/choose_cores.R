ad6ffacdc101b65265bb8e86df486cd3f60a9d2d
avahoffman <avamariehoffman@gmail.com>
2023-04-07 08:44:19 -0700

    Remove option to auto populate the cores

--- R/choose_cores.R
+++ R/choose_cores.R
@@ -9,134 +9,68 @@
 determine_cores <- function(input) {
   # Determine which raw data types belong to genomics..
   # TODO : do we want cores to appear with datatypes or vice versa?
+  #       Example of what an alternative would be commented below..
   antibody_tech_flag <-
-    "antibody_tech" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/antibody_tech.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/antibody_tech.yml")$tech_types
-    )
+    "antibody_tech" %in% input$core_datatype
+  # any(
+  #   input$raw_file_description %in% yaml.load_file("template/antibody_tech.yml")$raw_file_types
+  # ) |
+  # any(
+  #   input$technology_description %in% yaml.load_file("template/antibody_tech.yml")$tech_types
+  # )
   cell_img_flag <-
-    "cellular_imaging" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/cell_img.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/cell_img.yml")$tech_types
-    )
+    "cellular_imaging" %in% input$core_datatype
+
   em_tem_flag <-
     "em_tem" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/em_tem.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/em_tem.yml")$tech_types
-    # )
+
   em_sem_flag <-
     "em_sem" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/em_sem.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/em_sem.yml")$tech_types
-    # )
+
   em_cryo_flag <-
     "em_cryo" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/em_cryo.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/em_cryo.yml")$tech_types
-    # )
+
   eh_aperio_flag <-
-    "eh_aperio" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/eh_aperio.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/eh_aperio.yml")$tech_types
-    )
+    "eh_aperio" %in% input$core_datatype
+
   eh_polaris_flag <-
-    "eh_polaris" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/eh_polaris.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/eh_polaris.yml")$tech_types
-    )
+    "eh_polaris" %in% input$core_datatype
+
   eh_vectra_flag <-
-    "eh_vectra" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/eh_vectra.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/eh_vectra.yml")$tech_types
-    )
+    "eh_vectra" %in% input$core_datatype
+
   flow_cytometry_flag <-
-    "flow_cytometry" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/flow_cytometry.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/flow_cytometry.yml")$tech_types
-    )
+    "flow_cytometry" %in% input$core_datatype
+
   genomics_flag <-
-    "genomics" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/genomics.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/genomics.yml")$tech_types
-    )
+    "genomics" %in% input$core_datatype
+
   immune_flag <-
-    "immune" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/immune.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/immune.yml")$tech_types
-    )
+    "immune" %in% input$core_datatype
+
   large_animal_flag <-
-    "large_animal" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/large_animal.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/large_animal.yml")$tech_types
-    )
+    "large_animal" %in% input$core_datatype
+
   pi_ivis_flag <-
     "pi_ivis" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/pi_ivis.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/pi_ivis.yml")$tech_types
-    # )
+
   pi_microct_flag <-
     "pi_microct" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/pi_microct.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/pi_microct.yml")$tech_types
-    # )
+
   pi_mri_flag <-
     "pi_mri" %in% input$core_datatype
-    # any(
-    #   input$raw_file_description %in% yaml.load_file("template/pi_mri.yml")$raw_file_types
-    # ) |
-    # any(
-    #   input$technology_description %in% yaml.load_file("template/pi_mri.yml")$tech_types
-    # )
+
+  preclinical_model_flag <-
+    "preclinical_model" %in% input$core_datatype
+
   proteomics_flag <-
-    "proteomics" %in% input$core_datatype |
-    any(
-      input$raw_file_description %in% yaml.load_file("template/proteomics.yml")$raw_file_types
-    ) |
-    any(
-      input$technology_description %in% yaml.load_file("template/proteomics.yml")$tech_types
-    )
+    "proteomics" %in% input$core_datatype
+
+  small_animal_flag <-
+    "small_animal" %in% input$core_datatype
+
+  therapeutic_flag <-
+    "therapeutic" %in% input$core_datatype
 
   flags <- data.frame(
     "antibody_tech_flag" = antibody_tech_flag,
@@ -154,7 +88,10 @@ determine_cores <- function(input) {
     "pi_ivis_flag" = pi_ivis_flag,
     "pi_microct_flag" = pi_microct_flag,
     "pi_mri_flag" = pi_mri_flag,
-    "proteomics_flag" = proteomics_flag
+    "preclinical_model_flag" = preclinical_model_flag,
+    "proteomics_flag" = proteomics_flag,
+    "small_animal_flag" = small_animal_flag,
+    "therapeutic_flag" = therapeutic_flag
   )
 
   return(flags)
